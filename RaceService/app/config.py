27af4e40b9b0457d9c62aa1ec697c2c185e7c597
Kovacevic22 <vukasinkue03@gmail.com>
2026-05-29 13:04:26 +0200

    Implement project structure and configure database connection

---
+++ RaceService/app/config.py
@@ -0,0 +1,12 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=["../.env", ".env"],
+        extra="ignore",
+        env_file_encoding="utf-8"
+    )
+    database_url: str = Field(validation_alias="race_database_url")
+
+settings = Settings()
